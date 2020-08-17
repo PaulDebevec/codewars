e7dd33ad1cd6063f4e28c97a820d41b01e36a042
Paul Debevec <paulmdebevec@gmail.com>
2020-08-16 19:56:29 -0600

    Build tower recursion

--- build_tower/build_tower.rb
+++ build_tower/build_tower.rb
@@ -1,3 +1,14 @@
 def towerBuilder(n_floors)
-  return []
+  floors = []
+  if n_floors > 0
+    if n_floors % 2 == 0
+      floors << ("*" * (n_floors + 2))
+      towerBuilder(n_floors -= 1)
+    else
+      require "pry"; binding.pry
+      floors << ("*" * n_floors)
+      towerBuilder(n_floors -= 1)
+    end
+  end
+  floors
 end
