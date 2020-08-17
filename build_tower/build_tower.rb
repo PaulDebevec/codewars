d089f9881bfadbf39b50506c993367fd46b9a91b
Paul Debevec <paulmdebevec@gmail.com>
2020-08-16 19:57:06 -0600

    CHange recursion to iterative

--- build_tower/build_tower.rb
+++ build_tower/build_tower.rb
@@ -1,14 +1,17 @@
 def towerBuilder(n_floors)
-  floors = []
-  if n_floors > 0
-    if n_floors % 2 == 0
-      floors << ("*" * (n_floors + 2))
-      towerBuilder(n_floors -= 1)
-    else
-      require "pry"; binding.pry
-      floors << ("*" * n_floors)
-      towerBuilder(n_floors -= 1)
+  each_floor = (1..n_floors).to_a
+  spacing = (n_floors - 1)
+  tower = each_floor.map do |n|
+    # require "pry"; binding.pry
+    if n % 2 == 1
+    "*" * n + 1
     end
   end
-  floors
+  tower
 end
+
+# if n % 2 == 0 && n != 0
+#   "*" * (n + 2)
+# elsif n % 2 == 1
+#   "*" * n
+# end
