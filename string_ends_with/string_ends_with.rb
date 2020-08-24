ec38b8dbf5e61a02a563923e7b2699480f8d33c6
Paul Debevec <paulmdebevec@gmail.com>
2020-08-23 19:49:17 -0600

    Solution for string_ends_with kata

--- string_ends_with/string_ends_with.rb
+++ string_ends_with/string_ends_with.rb
@@ -1,3 +1,5 @@
 def solution(str, ending)
-  # TODO: complete
+  return true if ending == ""
+  index_1 = 0 - ending.length
+  str[index_1..-1] == ending
 end
--- string_ends_with/string_ends_with_test.rb
+++ string_ends_with/string_ends_with_test.rb
@@ -2,25 +2,26 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require_relative 'string_ends_with'
 
-class CompTest < Minitest::Test
+class SolutionTest < Minitest::Test
   def test_solution_returns_boolean
     assert_equal(true, solution('abc', 'bc'))
     assert_equal(true, solution('hello', 'llo'))
     assert_equal(false, solution('abc', 'd') )
     assert_equal(true, solution('hello world', 'world'))
+    assert_equal(true, solution('test space ', ' '))
   end
 end
 
 
 
-# SAMPLE TESTS
+### SAMPLE TESTS ###
 
 # TODO: Replace examples and use TDD by writing your own tests
 # These are some of the methods available:
 #   Test.expect(boolean, [optional] message)
 #   Test.assert_equals(actual, expected, [optional] message)
 #   Test.assert_not_equals(actual, expected, [optional] message)
-
+#
 # describe "Solution" do
 #   it "should test for something" do
 #     Test.assert_equals("actual", "expected", "This is just an example of how you can write your own TDD tests")
