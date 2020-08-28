0954dd39b8e7b27aab7e1cc03751f86057cc41f6
Paul Debevec <paulmdebevec@gmail.com>
2020-08-28 10:10:02 -0600

    Complete Sort The Odd kata

--- sort_the_odd/sort_the_odd.rb
+++ sort_the_odd/sort_the_odd.rb
@@ -1,3 +1,16 @@
 def sort_array(source_array)
-  #your code here
+  return [] if source_array.length == 1 && source_array.include?(nil)
+  source_array.delete(nil)
+  odd_array = source_array.select do |ele|
+    ele.odd?
+  end
+  odd_array.sort!
+  sorted_array = source_array.map do |element|
+    if element.even?
+      element
+    else
+      odd_array.shift
+    end
+  end
+  sorted_array
 end
